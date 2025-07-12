fcb4b02332bde3539e85ec0d883142721e7889f5
Kernel_panic <96293934+R4ph3@users.noreply.github.com>
2025-07-12 18:18:40 +0200

    Update script.js

--- script.js
+++ script.js
@@ -21,3 +21,22 @@ ruta.forEach((coord, index) => {
 });
 
 L.polyline(lineCoords, {color: 'blue'}).addTo(map);
+fetch('ubicaciones.json')
+  .then(response => response.json())
+  .then(ruta => {
+    const map = L.map('map').setView([ruta[0].lat, ruta[0].lng], 5);
+
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution: '© OpenStreetMap contributors'
+    }).addTo(map);
+
+    let lineCoords = [];
+    ruta.forEach((coord, index) => {
+      L.marker([coord.lat, coord.lng])
+        .addTo(map)
+        .bindPopup(`Fecha: ${coord.date}`);
+      lineCoords.push([coord.lat, coord.lng]);
+    });
+
+    L.polyline(lineCoords, { color: 'blue' }).addTo(map);
+  });
